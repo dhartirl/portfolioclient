f2e8dae1d55772208d161f1e1e98024b9353904a
David Hart <dhartirl@gmail.com>
2018-08-29 20:57:41 +0100

    Fix portfolio link, replace homepage image

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
-  providers: [ UserService ]
+  providers: [ UserService, RouterModule ]
 })
 export class NavigationComponent implements OnInit {
 
